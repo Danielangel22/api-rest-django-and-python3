b704d522ca839d9847b98143d6bc1a261ea61e98
Daniel Angel <trabajos.daniel2018@gmail.com>
2022-05-22 22:31:20 -0600

    initial commit

---
+++ api_app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import get_object_or_404, render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from api_app.models import CartItem
+from api_app.serializers import CartItemSerializer
+from rest_framework import status
+# Create your views here.
+
+
+class CartItemViews(APIView):
+    def post(self, request):
+        serializer = CartItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=200)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=400)
+
+    def get(self, request, id=None):
+        if id:
+            item = CartItem.objects.get(id=id)
+            serializer = CartItemSerializer(item)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+        items = CartItem.objects.all()
+        serializer = CartItemSerializer(items, many=True)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+    def patch(self, request, id=None):
+        item = CartItem.objects.get(id=id)
+        serializer = CartItemSerializer(item, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+        else:
+            return Response({"status": "error", "data": serializer.errors})
+
+    def delete(self, request, id=None):
+        item = get_object_or_404(CartItem, id=id)
+        item.delete()
+        return Response({"status": "success", "data": "Item Deleted"})
